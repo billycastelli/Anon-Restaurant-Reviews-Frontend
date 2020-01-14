a87da0d03d0450205d61723aa13abd918a5daf22
William Castelli <billycastelli@gmail.com>
2020-01-13 23:06:39 -0800

    Basic search, calls to API Gateway

---
+++ src/App.js
@@ -0,0 +1,302 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+export class NavBar extends React.Component {
+    state = {};
+    render() {
+        return (
+            <nav
+                className="navbar is-danger"
+                role="navigation"
+                aria-label="main navigation"
+            >
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="/about.html">
+                        <img
+                            src="NobodysOpinion_icon.png"
+                            width="30"
+                            height="30"
+                        />
+                    </a>
+
+                    <a
+                        role="button"
+                        className="navbar-burger burger"
+                        aria-label="menu"
+                        aria-expanded="false"
+                        data-target="navbarBasicExample"
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                        <a className="navbar-item">Home</a>
+                    </div>
+                    <div className="navbar-menu">
+                        <div className="navbar-item field has-addons">
+                            <div className="control has-icons-left">
+                                <input
+                                    className="input"
+                                    type="email"
+                                    placeholder="Search for restaurants"
+                                />
+                                <span className="icon is-small is-left">
+                                    <i className="fa fa-search"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+class HeaderText extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1 className="title is-1" style={{ textAlign: "center" }}>
+                    [Site Title]
+                </h1>
+                <p className="subtitle" style={{ textAlign: "center" }}>
+                    A truly anonymous way to rate and review restaurants
+                </p>
+            </React.Fragment>
+        );
+    }
+}
+
+class Search extends React.Component {
+    constructor() {
+        super();
+        this.onSubmit = this.onSubmit.bind(this);
+        this.checkEnter = this.checkEnter.bind(this);
+    }
+
+    state = {
+        userInput: null
+    };
+
+    onSubmit() {
+        let newInput = document.getElementById("searchInput").value;
+        // if userInput is empty, error message
+        if (newInput.length == 0) {
+            return;
+        } else {
+            this.setState({ userInput: newInput });
+            console.log(newInput);
+        }
+    }
+
+    checkEnter(e) {
+        if (e.key === "Enter") {
+            this.onSubmit();
+        }
+    }
+
+    render() {
+        let search = null;
+        if (this.state.userInput !== null && this.state.userInput.length > 0) {
+            search = <SearchResults query={this.state.userInput} />;
+        }
+        return (
+            <React.Fragment>
+                <section className="section">
+                    <div className="container">
+                        <div className="columns is-centered">
+                            <div className="column is-half">
+                                <HeaderText />
+                                <div className="field has-addons has-addons-centered">
+                                    <div className="control">
+                                        <input
+                                            className="input"
+                                            type="text"
+                                            id="searchInput"
+                                            placeholder="Search for restaurants"
+                                            onKeyPress={this.checkEnter}
+                                        />
+                                    </div>
+                                    <div className="control">
+                                        <a
+                                            className="button is-info"
+                                            onClick={this.onSubmit}
+                                        >
+                                            Search
+                                        </a>
+                                    </div>
+                                </div>
+                                <div
+                                    className="container"
+                                    style={{ textAlign: "center" }}
+                                >
+                                    <button className="button">
+                                        Show local restaurants
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                {search}
+            </React.Fragment>
+        );
+    }
+}
+
+class SearchResults extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.getResultsServer = this.getResultsServer.bind(this);
+        this.getResultsServer(this.props.query);
+    }
+
+    state = {
+        allResults: {}
+    };
+
+    getResultsServer(query) {
+        console.log("Request sent");
+        let BASE_URL =
+            "https://4jb0iea9tb.execute-api.us-west-2.amazonaws.com/prod/search?";
+        let FULL_URL =
+            BASE_URL + "query=" + query + "&lat=33.158092&lon=-117.350594";
+        fetch(FULL_URL, {
+            method: "GET",
+            mode: "cors"
+        })
+            .then(Response => {
+                return Response.json();
+            })
+            .then(jsonData => {
+                console.log(jsonData);
+                this.setState({ allResults: jsonData });
+            });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.query !== this.props.query) {
+            this.getResultsServer(this.props.query);
+        }
+    }
+
+    render() {
+        let rowsDisplay = null;
+        if (this.state.allResults.restaurants) {
+            let restaurants = this.state.allResults.restaurants;
+            let rows = [];
+            //console.log(restaurants.length);
+
+            // Display 4 restaurants at a time
+            let toDisplay = [];
+            for (let i = 0; i < restaurants.length; i++) {
+                toDisplay.push(restaurants[i]);
+                // Clear display array after 4 filled
+                if (toDisplay.length % 4 == 0) {
+                    rows.push(<ResultRow key={i} toDisplay={toDisplay} />);
+                    toDisplay = [];
+                }
+            }
+            if (rows.length == 0) {
+                rows.push(<ResultRow key={0} toDisplay={restaurants} />);
+            }
+            rowsDisplay = <React.Fragment>{rows}</React.Fragment>;
+        }
+        return (
+            <section className="section">
+                <div className="container">
+                    <h1 className="title">Search results</h1>
+                    {rowsDisplay}
+                </div>
+            </section>
+        );
+    }
+}
+
+class ResultRow extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {};
+    render() {
+        let cards = [];
+        for (let i = 0; i < this.props.toDisplay.length; i++) {
+            cards.push(
+                <ResultCard key={i} restaurant={this.props.toDisplay[i]} />
+            );
+        }
+        return <div className="columns">{cards}</div>;
+    }
+}
+
+class ResultCard extends React.Component {
+    constructor(props) {
+        super(props);
+        // console.log(this.props.restaurant);
+    }
+
+    render() {
+        let restaurant = this.props.restaurant.restaurant;
+        console.log(restaurant);
+        let photo = "https://bulma.io/images/placeholders/1280x960.png";
+        if (restaurant.photos) {
+            photo = restaurant.photos[0].photo.url;
+        }
+        return (
+            <div className="column">
+                <div className="card">
+                    <div className="card-image">
+                        <figure className="image is-4by3">
+                            <img src={photo} alt="Restaurant image" />
+                        </figure>
+                    </div>
+                    <div className="card-content">
+                        <div className="media">
+                            <div className="media-content">
+                                <p className="title is-4">{restaurant.name}</p>
+                            </div>
+                        </div>
+
+                        <div className="content">
+                            <b>Cuisine</b>
+                            <br />
+                            {restaurant.cuisines}
+                            <br />
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Phasellus nec iaculis mauris.
+                            <a>@bulmaio</a>. <a href="#">#css</a>
+                            <a href="#">#responsive</a>
+                            <br />
+                            <b>Address</b>
+                            <br />
+                            {restaurant.location.address}
+                            <br />
+                            <b>Number</b>
+                            <br />
+                            {restaurant.phone_numbers}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    state = {};
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <Search />
+            </div>
+        );
+    }
+}
+export default App;
